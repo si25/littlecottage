154a71079f75e28037cf45321cb21bcdd9e2893c
si25 <simon-gibbs@hotmail.com>
2020-09-11 14:26:21 +0100

    add all image, edit layout, change colour scheme

--- js/script.js
+++ js/script.js
@@ -47,19 +47,34 @@ $("#nb .nav-item").click(function(event) {
   var closeHeader = document.getElementsByClassName('navbar-brand')[0];
 
 
+function disableScroll() { 
+    // Get the current page scroll position 
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop; 
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft, 
+  
+      // if any scroll is attempted, set this to the previous value 
+    window.onscroll = function() { 
+      window.scrollTo(scrollLeft, scrollTop); 
+    }; 
+} 
+  
+function enableScroll() { 
+    window.onscroll = function() {}; 
+} 
 
 
-console.log(modalContainer);
 
 for(var i = 0; i <btn.length; i++) { 		// Opens booking modal //
   var toCLick = btn[i]; 
   toCLick.onclick = function() {
   	modal.style.display = 'block';
+    disableScroll();
   }
 }
 
 amBtn.onclick = function() {				// Opens Amenities modal //
 	amModal.style.display = 'block';
+  disableScroll();
 }
 
 
@@ -68,6 +83,7 @@ for(var i = 0; i <close.length; i++) { 		// Closes all modals //
   toCLick.onclick = function() {
   	modal.style.display = 'none';
   	amModal.style.display ='none';
+    enableScroll();
   }
 }
 
@@ -77,6 +93,7 @@ for(var i = 0; i < modalContainer.length; i++) {  // Closes all modals when area
     if (event.target == modal || event.target == amModal){
         modal.style.display = 'none';
         amModal.style.display ='none';
+        enableScroll();
     }
   }
 }
@@ -86,14 +103,15 @@ for(var i = 0; i <closeNav.length; i++) {	// Closes all modals on Nav-bar (Fix f
   var toCLick = closeNav[i]; 
   toCLick.onclick = function() {
   	modal.style.display = 'none';
-  	amModal.style.display = 'none'; 
+  	amModal.style.display = 'none';
+    enableScroll();
   }
 }
 
 closeHeader.onclick = function() {
 	modal.style.display = 'none';
   amModal.style.display = 'none'; 
-
+  enableScroll();
 }
 
 
@@ -101,16 +119,22 @@ closeHeader.onclick = function() {
 
 /** Gallery button open **/ 
 
-var galBtn = document.getElementsByClassName('gal-btn')[0];
-var firstImage = document.getElementsByClassName('gimg')[0];
-
-console.log(firstImage);
-console.log(galBtn);
+var galBtn = document.getElementsByClassName('gal-btn');
+var firstImage = document.getElementsByClassName('gimg');
+var galleryClose = document.getElementsByClassName('lb-close');
 
 
-galBtn.onclick = function() {
-  firstImage.click();
+console.log(galleryClose);
+
+
+
+galBtn[0].onclick = function() {
+  firstImage[0].click();
 }
+
+
+
+
 
 
 
